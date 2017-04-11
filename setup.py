2d02a18f2f8d5232fcd1ce6f20cfaa6d03dbbb42
Marco Bosio <marco.bosio@axant.it>
2017-04-11 16:30:27 +0200

    Tweak setup

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 install_requires=[
     "configparser",
@@ -19,14 +16,14 @@ setup(description='ContactHub SDK Python',
       url='https://github.com/axant/contacthub-sdk-python',
       version='0.1',
       install_requires=install_requires,
-      packages=['contacthub'],
+      packages=find_packages(exclude=['tests', 'tests.*']),
       extras_require={
           'testing': testpkgs,
           'documentation': ['Sphinx==1.4.1']
       },
       scripts=[],
       name='contacthub',
-      include_package_data=True,
+      include_package_data=False,
       zip_safe=False,
       test_suite='nose.collector',
       tests_require=testpkgs,
